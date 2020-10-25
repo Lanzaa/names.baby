efad15435ff1617912145eca3b5d0415c853c06c
Colin B <lanzaa@gmail.com>
2020-10-24 21:50:11 -0700

    Build

---
+++ docs/precache-manifest.2ef03b03a8cddd9816727cec31715641.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bbab41deb649eabdcf06987684b79bf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d831621f66e5d454c603",
+    "url": "/static/css/main.4b8f31b7.chunk.css"
+  },
+  {
+    "revision": "228b933b5939eefc353b",
+    "url": "/static/js/2.ccced9fa.chunk.js"
+  },
+  {
+    "revision": "fd0d68870237ed86b2ff153be3348a6a",
+    "url": "/static/js/2.ccced9fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d831621f66e5d454c603",
+    "url": "/static/js/main.86a7df6e.chunk.js"
+  },
+  {
+    "revision": "f93abbfba15ffd94a266",
+    "url": "/static/js/runtime-main.49dadacd.js"
+  }
+]);
